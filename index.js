99e86d12fa243ed7e024e7de83a01f6430f1a424
hxkim10 <abdelhakimtiabi@gmail.com>
2023-10-01 06:53:45 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  main.innerHTML = '';
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
